1c7edb47a367aed473493cab60bf38ba6a880ad5
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-03-03 18:56:30 +0530

    create functions to set music album recycler view, music adapter.

--- app/src/main/java/com/example/uidemo/MainActivity.kt
+++ app/src/main/java/com/example/uidemo/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.uidemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html.fromHtml
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import musicPlaylistAdapter.MusicAdapter
+import musicPlaylistModel.MusicPlaylist
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -19,5 +24,55 @@ class MainActivity : AppCompatActivity() {
 
         val materialToolbar: MaterialToolbar = findViewById(R.id.mt_toolbar)
         setSupportActionBar(materialToolbar)
+
+
+        val musicAlbumRecyclerView: RecyclerView = setUpMusicAlbumRecyclerView()
+        val musicPlaylist = createMusicPlaylist()
+        setMusicAdapter(musicPlaylist, musicAlbumRecyclerView)
+
     }
+
+    private fun setMusicAdapter(
+        musicPlaylist: List<MusicPlaylist>,
+        musicAlbumRecyclerView: RecyclerView
+    ) {
+        val musicAdapter = MusicAdapter(musicPlaylist)
+        musicAlbumRecyclerView.adapter = musicAdapter
+    }
+
+    private fun setUpMusicAlbumRecyclerView(): RecyclerView {
+        val musicAlbumRecyclerView: RecyclerView = findViewById(R.id.music_album_rv)
+        musicAlbumRecyclerView.setHasFixedSize(true)
+        musicAlbumRecyclerView.layoutManager = LinearLayoutManager(this)
+        return musicAlbumRecyclerView
+    }
+
+    private fun createMusicPlaylist(): List<MusicPlaylist> {
+        val musicPlaylist = listOf(
+            MusicPlaylist(
+                R.drawable.psyco_music_image,
+                getString(R.string.psyco_music_name_text_view),
+                getString(R.string.psyco_music_singer_name)
+            ),
+            MusicPlaylist(
+                R.drawable.at_my_worst_music_image,
+                getString(R.string.at_my_worst_music_name_text_view),
+                getString(R.string.at_my_worst_music_singer_name)
+            ),
+            MusicPlaylist(
+                R.drawable.at_my_worst_music_image,
+                getString(R.string.at_my_worst_music_name_text_view),
+                getString(R.string.at_my_worst_music_singer_name)
+            ),
+            MusicPlaylist(
+                R.drawable.psyco_music_image,
+                getString(R.string.psyco_music_name_text_view),
+                getString(R.string.psyco_music_singer_name)
+            )
+
+        )
+        return musicPlaylist
+    }
+
+
 }
